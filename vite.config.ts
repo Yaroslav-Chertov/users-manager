c074f5a6b7f6cbe0c92e510b479e988a17dc503c
Yaroslav-Chertov <yachertov@yandex.ru>
2025-11-17 11:52:59 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+const isGithub = process.env.GITHUB_PAGES === "true";
+
+export default defineConfig({
   plugins: [react()],
-  base: "/",
-}));
+  base: isGithub ? "/users-manager/" : "/",
+});
